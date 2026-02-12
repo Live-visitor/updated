e676c7eea812cf05d7308ad3d701ca1592270b68
Live-visitor <reshyrules@gmail.com>
2026-02-12 17:58:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,10 +24,6 @@ def serve_page(path):
         file_path = path
     
 
-        except:
-            return "Page not found", 404
-
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5010, debug=True, threaded=True)
 
